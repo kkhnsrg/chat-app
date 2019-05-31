0f6cb079e579d563599c121bdd5f7d0f1a192f27
kokhansergey <m.sekohan@gmail.com>
2019-05-31 10:14:01 +0300

    Sendeing messages is added

--- app/src/main/java/edu/kokhan/simplechat/MainActivity.kt
+++ app/src/main/java/edu/kokhan/simplechat/MainActivity.kt
@@ -2,21 +2,44 @@ package edu.kokhan.simplechat
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    val SIGN_IN_REQUEST_CODE = 1
-    //adapter firebase
+    private val database = FirebaseDatabase.getInstance()
+    private val messagesRef = database.getReference("messages")
+    private val MAX_MESSAGE_LENGTH = 150
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener{
-//            editText
+        button.setOnClickListener {
+            val message = editTextMessage.text.toString()
+
+            if(!messageIsCorrect(message)) return@setOnClickListener
+
+            messagesRef.push().setValue(message)
+            editTextMessage.setText("")
         }
+    }
+
+    private fun messageIsCorrect(message: String): Boolean {
+        var correctResult = true
+        if (message == "") {
+            Toast.makeText(this, "Message input is empty!", Toast.LENGTH_SHORT).show()
+            correctResult = false
+        }
+
+        if (message.length > MAX_MESSAGE_LENGTH) {
+            Toast.makeText(this, "Message text cannot contains more than $MAX_MESSAGE_LENGTH symbols!", Toast.LENGTH_SHORT).show()
+            correctResult = false
+        }
+
+        return correctResult
     }
 
 
